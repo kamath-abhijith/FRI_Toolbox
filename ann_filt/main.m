3a41835840c8c33e87115e13b2f5aa58dc45f7fb
kamath-abhijith <kamath.abhijith@gmail.com>
2019-11-07 14:39:29 +0530

    checkup

--- ann_filt/main.m
+++ ann_filt/main.m
@@ -2,33 +2,63 @@ clear
 clc
 close all
 
-%% Support
-n = 1:49; N = length(n);
-dt = 0.1; t = 0:dt:49; Nt = length(t);
+%% Support 
+dt = 1e-3; t = 0:dt:1; T0 = max(t);
+Nt = length(t); n = 1:Nt;
 
-%% Weights
-spden = 0.1;
-x = full(sprandn(1,N,spden));
+%% Define FRI signal
+% Set weights and delays
+al = [1; -2; -1; 3];
+taul = [0.1; 0.3; 0.6; 0.7];
+taul_idx = floor(Nt*taul/T0);
+L = length(al);
 
-%% Sinc interpolation // Sampling kernel
-[Ts,T] = ndgrid(t,n);
-y = sinc(Ts - T)*x';
+% Construct the signal
+x = zeros(Nt,1);
+x(taul_idx) = al;
 
 %% Sampling
-nn = 1:4:Nt;
+% Sampling kernel
+tlpf = -1:dt:1;
+lpf = sinc(100*tlpf);
+y = conv(x,lpf,'same');
+
+% Uniform sampling
+nn = 1:10:Nt;
 yn = y(nn);
+Nk = floor(length(yn)/2);
 
-%% HRSE from SWCE form
+%% Parameter estimation
+% Estimation of delays
 haty = fft(yn);
-L = ceil(spden*N);
-wk = ann_filt(haty,ceil(L));
+wk = ann_filt(haty,L);
+est_taul = -T0*wk/(2*pi);
 
-tk = N*wk./(2*pi);
+% Estimation of amplitudes
+E = exp(1j*(-Nk:Nk)'*wk');
+est_al = T0*pinv(E)*haty;
+est_al = sign(angle(est_al)).*abs(est_al);
+
+%% Reconstruction
+xrec = zeros(Nt,1);
+est_taul_idx = floor(Nt*est_taul/T0);
+xrec(est_taul_idx) = est_al;
 
 %% Plots
-figure
-stem(n,x,'-g',"LineWidth",1)
-hold on, grid on
-plot(t,y,'-b',"LineWidth",2)
-stem(t(nn),yn,'-r',"LineWidth",2)
-axis([0 max(t) -1.2*max(-min(x),max(x)) 1.2*max(-min(x),max(x))])
+figure, subplot(2,1,1)
+plot(t,y,'-g',"LineWidth",2)
+grid on, hold on
+stem(t(nn),yn,'-b')
+axis([0 T0 -max(-min(x),max(x))-0.5 max(-min(x),max(x))+0.5])
+xlabel('$t$','Interpreter','latex')
+ylabel('$x(t)$','Interpreter','latex')
+title('Sampling','Interpreter','latex')
+
+subplot(2,1,2)
+stem(t,x,'-g',"LineWidth",2)
+grid on, hold on
+stem(t,xrec,'-b',"LineWidth",2)
+axis([0 T0 -max(-min(x),max(x))-0.5 max(-min(x),max(x))+0.5])
+xlabel('$t$','Interpreter','latex')
+ylabel('$x(t)$','Interpreter','latex')
+title('Reconstruction','Interpreter','latex')
